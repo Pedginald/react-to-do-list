52f3496d1db04968fca40ab89d3b3b3b4f6a83a9
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-10 17:15:09 -0400

    Creating seperate component for list item

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ToDoItem() {
+	return (
+		<div className='todo-item'>
+			<input type='checkbox'></input><p>Placeholder</p>
+		</div>
+	)
+}
+
+export default ToDoItem;
