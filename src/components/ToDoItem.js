c808e3a94e00f281e431c31565537230e69c5c77
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-11 17:08:10 -0400

    Testing onchange function

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ToDoItem(props) {
 	return (
 		<div className='todo-item'>
-			<input type='checkbox' checked={props.item.completed}></input>
+			<input type='checkbox' checked={props.item.completed} onChange={() => console.log('clicked')}></input>
 			<p>{props.item.name}</p>
 		</div>
 	)
