0250d24a45a8fc469268b95e6e8f1afead1dc53c
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-11 13:56:10 -0400

    Creating a map function to display each to do item

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ToDoItem from './ToDoItem.js'
+import ToDoItem from './ToDoItem.js';
+import toDoData from '../toDoData';
 
 function MainContent() {
+	const toDoComponent = toDoData.map(item => <ToDoItem key={item.id} item={item}/>)
 	return (
 		<div>
 			<h1>To do list</h1>
 			<p></p>
 			<form className='todo-list'>
-				<ToDoItem />
-				<ToDoItem />
-				<ToDoItem />
-				<ToDoItem />
+				{toDoComponent}
 			</form>
 		</div>
 	)
