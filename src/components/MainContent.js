4913d1d54088559ab5d4d94f78ad7f49534fe9a3
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-11 17:07:49 -0400

    Introducing state and classes to main content

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ToDoItem from './ToDoItem.js';
 import toDoData from '../toDoData';
 
-function MainContent() {
-	const toDoComponent = toDoData.map(item => <ToDoItem key={item.id} item={item}/>)
-	return (
-		<div>
-			<h1>To do list</h1>
-			<p></p>
-			<form className='todo-list'>
-				{toDoComponent}
-			</form>
-		</div>
-	)
+class MainContent extends Component {
+	constructor() {
+		super()
+		this.state = {
+			todos: toDoData
+		}
+	}
+
+	render() {
+		const toDoComponent = this.state.todos.map(item => <ToDoItem key={item.id} item={item}/>)
+		return (
+			<div>
+				<h1>To do list</h1>
+				<form className='todo-list'>
+					{toDoComponent}
+				</form>
+			</div>
+		)
+	}
 }
 
 export default MainContent;
