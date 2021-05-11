9a1a0d368e5a502d68f154100016ec50ee84ba5d
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-11 14:30:23 -0400

    Adding completed boolean to tododata

--- src/toDoData.js
+++ src/toDoData.js
@@ -1,18 +1,29 @@
 const toDoData = [
 	{
 		id: '1',
-		name: 'Wash the car'
+		name: 'Wash the car',
+		completed: true
 	},
 	{
 		id: '2',
-		name: 'Go shopping'
+		name: 'Go shopping',
+		completed: false
 	},
 	{
 		id: '3',
-		name: 'Pack bags for holiday'
+		name: 'Pack bags for holiday',
+		completed: true
 	},
 	{
 		id: '4',
-		name: 'File paperwork'
+		name: 'File paperwork',
+		completed: false
+	},
+	{
+		id: '5',
+		name: 'Walk the dog',
+		completed: true
 	}
 ]
+
+export default toDoData;
