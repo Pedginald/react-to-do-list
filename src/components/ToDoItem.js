d4e590a3af03cefd116033e85c6fd664268f2682
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-11 14:32:14 -0400

    Adding props to to do items

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function ToDoItem() {
+function ToDoItem(props) {
 	return (
 		<div className='todo-item'>
-			<input type='checkbox'></input><p>Placeholder</p>
+			<input type='checkbox' checked={props.item.completed}></input>
+			<p>{props.item.name}</p>
 		</div>
 	)
 }
