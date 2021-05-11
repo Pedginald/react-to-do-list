fb6266b636d2d5267d7b3c4c1d2c532aacc4b5e3
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-11 13:55:48 -0400

    Creating a to do data store

---
+++ src/toDoData.js
@@ -0,0 +1,18 @@
+const toDoData = [
+	{
+		id: '1',
+		name: 'Wash the car'
+	},
+	{
+		id: '2',
+		name: 'Go shopping'
+	},
+	{
+		id: '3',
+		name: 'Pack bags for holiday'
+	},
+	{
+		id: '4',
+		name: 'File paperwork'
+	}
+]
