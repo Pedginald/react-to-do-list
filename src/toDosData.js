466364ade3c428f56e9e80e8b0f9b3df0e856951
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-12 15:06:23 -0400

    Adding to do list file

---
+++ src/toDosData.js
@@ -0,0 +1,29 @@
+const toDoData = [
+	{
+		id: '1',
+		name: 'Wash the car',
+		completed: true
+	},
+	{
+		id: '2',
+		name: 'Go shopping',
+		completed: false
+	},
+	{
+		id: '3',
+		name: 'Pack bags for holiday',
+		completed: true
+	},
+	{
+		id: '4',
+		name: 'File paperwork',
+		completed: false
+	},
+	{
+		id: '5',
+		name: 'Walk the dog',
+		completed: true
+	}
+]
+
+export default toDoData;
