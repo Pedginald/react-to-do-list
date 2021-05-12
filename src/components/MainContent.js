d65afb9c16200143297ec7310551a96c3c784168
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-05-12 15:07:08 -0400

    Adding function to handle completion state

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-import ToDoItem from './ToDoItem.js';
-import toDoData from '../toDoData';
+import ToDoItem from './ToDoItem';
+import toDosData from '../toDosData';
 
 class MainContent extends Component {
 	constructor() {
 		super()
 		this.state = {
-			todos: toDoData
+			todos: toDosData
 		}
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(id) {
+		this.setState(prevState => {
+			const upDatedTodos = prevState.todos.map(item => {
+				if (item.id === id) {
+					item.completed = !item.completed
+				}
+				return item;
+			})
+			return {
+				todos: upDatedTodos
+			}
+		})
 	}
 
 	render() {
-		const toDoComponent = this.state.todos.map(item => <ToDoItem key={item.id} item={item}/>)
+		const toDosComponent = this.state.todos.map(item => {
+			return <ToDoItem 
+				key={item.id} 
+				item={item}
+				handleChange={this.handleChange}
+			/>
+		})
+
 		return (
 			<div>
 				<h1>To do list</h1>
 				<form className='todo-list'>
-					{toDoComponent}
+					{toDosComponent}
 				</form>
 			</div>
 		)
